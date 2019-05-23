6ab77fabf6401da95ca929748f8a22ab86bd8caa
Marlon Monzon <martianworm17@gmail.com>
2019-05-23 22:05:20 +0800

    Add insert method

--- src/com/structures/linkedlist/SinglyLinkedList.java
+++ src/com/structures/linkedlist/SinglyLinkedList.java
@@ -2,7 +2,21 @@ package com.structures.linkedlist;
 
 public class SinglyLinkedList {
 
+    Node start;
+
     public static void main() {
 
+    }
+
+    public void insert(int value) {
+        Node current = start;
+
+        while(current.next != null) {
+            current = current.next;
+        }
+
+        Node newNode = new Node(value);
+
+        current.next = newNode;
     }
 }
